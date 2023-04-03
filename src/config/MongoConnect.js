eebca300f7e3b31c5464f4ee40096fff4dc24711
HaiNguyen <bongnghin5@gmail.com>
2023-04-03 20:41:13 +0700

    config MongoDB

--- src/config/MongoConnect.js
+++ src/config/MongoConnect.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/CinemaSystem');
+        await mongoose.connect('mongodb://mongo:xMm9q9TLxGTYfEHEm69a@containers-us-west-52.railway.app:7136');
         console.log("Connected");
     } catch (error) {
         console.log(error);
