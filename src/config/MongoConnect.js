dc29e684025408c9344102aeb993171c2e6baf6c
HaiNguyen <bongnghin5@gmail.com>
2023-05-03 08:02:35 +0700

    config MongoDB

--- src/config/MongoConnect.js
+++ src/config/MongoConnect.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://hainguyen:j0ywL9C6NN7d4qN0@cluster0.a0gabxx.mongodb.net/cts');
+        await mongoose.connect('mongodb://mongo:t7Bt1eUd5BMnMRdRXlXG@containers-us-west-54.railway.app:7575');
         console.log("Connected");
     } catch (error) {
         console.log(error);
