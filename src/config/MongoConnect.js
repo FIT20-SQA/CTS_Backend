d2457dbbb1474c68af57bc7ce8e34b038a84e1a7
HaiNguyen <bongnghin5@gmail.com>
2023-05-19 18:38:46 +0700

    change database url

--- src/config/MongoConnect.js
+++ src/config/MongoConnect.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 export default async function connect() {
     try {
-        // await mongoose.connect('mongodb://mongo:t7Bt1eUd5BMnMRdRXlXG@containers-us-west-54.railway.app:7575');
-        await mongoose.connect('mongodb://localhost:27017/cts');
+        await mongoose.connect('mongodb://mongo:gWXmSMcgGPtPSKnHrrKZ@containers-us-west-17.railway.app:6741');
+        // await mongoose.connect('mongodb://localhost:27017/cts');
         console.log("Connected");
     } catch (error) {
         console.log(error);
